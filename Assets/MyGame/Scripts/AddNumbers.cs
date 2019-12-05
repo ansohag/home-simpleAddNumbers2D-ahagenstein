95540687720c15efa1a5ecc3c74d6871c3658797
ansohag <anna.hagenstein@htl-salzburg.ac.at>
2019-12-05 18:29:16 +0100

    added Script, Add Button works, reset button in progress

---
+++ Assets/MyGame/Scripts/AddNumbers.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AddNumbers : MonoBehaviour
+{
+
+    public InputField ipVarA;
+    public InputField ipVarB;
+    public Text result;
+
+    float varA;
+    float varB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetResult();
+    }
+
+    public void SetResult()
+    {
+        float a = float.Parse(ipVarA.text);
+        float b = float.Parse(ipVarB.text);
+        Debug.Log("in SetResult " + (a + b));
+        float resultAB = a + b;
+        result.text = resultAB.ToString();
+        //result.text = SubstractNumbers(a,b);
+    }
+}
